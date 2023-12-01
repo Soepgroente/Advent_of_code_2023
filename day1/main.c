59827a292effb843e682eb4a1e4fb6e55e2e0adf
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2023-12-01 15:01:52 +0100

    day 1

---
+++ day1/main.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int	compare_input(char* input)
+{
+	char c = *input;
+
+	if (c >= '1' && c <= '9')
+		return ((c - '0'));
+	if (strncmp(input, "one", 3) == 0)
+		return (1);
+	if (strncmp(input, "two", 3) == 0)
+		return (2);
+	if (strncmp(input, "three", 5) == 0)
+		return (3);
+	if (strncmp(input, "four", 4) == 0)
+		return (4);
+	if (strncmp(input, "five", 4) == 0)
+		return (5);
+	if (strncmp(input, "six", 3) == 0)
+		return (6);
+	if (strncmp(input, "seven", 5) == 0)
+		return (7);
+	if (strncmp(input, "eight", 5) == 0)
+		return (8);
+	if (strncmp(input, "nine", 4) == 0)
+		return (9);
+	return (0);
+}
+
+int	convert_str_to_int(char* input)
+{
+	int	i = 0; int res = 0;
+
+	while (input[i] != '\0' && compare_input(&input[i]) == 0)
+		i++;
+	res += compare_input(&input[i]) * 10;
+	i = strlen(input) - 1;
+	while (i > 0 && compare_input(&input[i]) == 0)
+		i--;
+	res += compare_input(&input[i]);
+	return (res);
+}
+
+int main(int argc, char** argv)
+{
+	int result = 0;
+
+	(void)argc;
+	for (int i = 1; argv[i] != NULL; i++)
+		result += convert_str_to_int(argv[i]);
+	printf("Result: %d\n", result);
+}
