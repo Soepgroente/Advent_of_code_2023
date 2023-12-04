35b63372f38ab662694cd7b6c7f5fb90036f248c
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2023-12-05 00:41:19 +0100

    updates or something

---
+++ day3/main.c
@@ -0,0 +1,24 @@
+#include "day3.h"
+
+int main(void)
+{
+	FILE* 	file;
+	char* 	tmp;
+	char**	input;
+	int 	size;
+
+	file = fopen("input.txt", "r");
+	fseek(file, 0, SEEK_END);
+	size = ftell(file);
+	rewind(file);
+	tmp = calloc((size + 1), sizeof(char));
+	fread(tmp, sizeof(char), size, file);
+
+	input = ft_split(tmp, '\n');
+	if (input == NULL)
+		return (1);
+
+	silver_day4(input);
+	gold_day4(input);
+	fclose(file);
+}
